63e0c2ac011c323223269b64d2c1927d481d1fa1
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-02-22 21:21:09 +0100

    update functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 import memories from './images/memories.png'
 import { getPosts } from './actions/posts'
@@ -7,7 +7,10 @@ import { useDispatch } from 'react-redux'
 import Form from './components/Form/Form'
 import Posts from './components/Posts/Posts'
 import useStyles from './styles'
+
+
 export const App = () => {
+    const [currentId, setCurrentId] = useState(null)
     const dispatch = useDispatch()
     const classes=useStyles()
 
@@ -15,7 +18,7 @@ export const App = () => {
 
     useEffect( () => {
         dispatch(getPosts())
-    }, [dispatch] )
+    }, [ currentId ,dispatch] )
 
 
     return (
@@ -30,10 +33,10 @@ export const App = () => {
         <Container>
             <Grid container justify="space-between" alignItems='stretch' spacing={3}>
             <Grid item xs={12} sm={7} >
-            <Posts />
+            <Posts setCurrentId={setCurrentId} />
             </Grid>
             <Grid item xs={12} sm={4} >
-                <Form />
+                <Form currentId={currentId} setCurrentId={setCurrentId} />
                 </Grid>
             </Grid>
         </Container>
--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,8 @@ import thunk from 'redux-thunk'
 import reducers from './reducers'
 import App from './App'
 
+import './index.css'
+
 const store = createStore(reducers, compose(applyMiddleware(thunk)) )
 ReactDom.render( 
     <Provider store={store} >
