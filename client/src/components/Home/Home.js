832d9ff6cabcb265a89ad4f8282cf2c2d2f71c43
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-02-28 22:56:38 +0100

    loading

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -77,9 +77,12 @@ const Home = () => {
            </Button>
           </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
-            <Paper  elevation={6} >
+            {(!searchQuery && !tags.length)&& (
+              <Paper  elevation={6} className={classes.pagination} >
             <Pagination page={page} />
             </Paper>
+            )}
+  
           </Grid>
         </Grid>
       </Container>
