7de9012a518dbd50dd70c7e40f81cf724df27545
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-03-01 22:38:20 +0100

    small fixes

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -42,11 +42,13 @@ export const getPostsBySearch = (searchQuery) => async (dispatch) => {
   }
 }
 
-export const createPost = (post) => async (dispatch) => {
+export const createPost = (post, history) => async (dispatch) => {
   try {
     dispatch({ type: START_LOADING })
 
     const { data } = await api.createPost(post);
+
+    history.push(`/posts/${data._id}`)
 
     dispatch({ type: CREATE, payload: data });
   } catch (error) {
