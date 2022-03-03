902f227a2a31574e0d2a10db657c4477b3e02e47
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-03-03 21:50:51 +0100

    fetching comments

--- client/src/components/PostDetails/CommentSection.jsx
+++ client/src/components/PostDetails/CommentSection.jsx
@@ -8,17 +8,20 @@ import useStyles from './styles'
 
 const CommentSection = ({ post }) => {
     const classes = useStyles()
-    const [comments, setComments] = useState([1, 2, 3, 4])
+    const [comments, setComments] = useState(post?.comments)
     const [comment, setComment] = useState('')
     const user = JSON.parse(localStorage.getItem('profile'))
     const dispatch = useDispatch()
 
 
 
-    const handleClick=()=> {
+    const handleClick= async () => {
         const finalComment = `${user.result.name}: ${comment}`
 
-        dispatch(commentPost(finalComment, post._id))
+        const newComments = await dispatch(commentPost(finalComment, post._id))
+
+        setComments(newComments)
+        setComment('')
     }
 
   return (
@@ -28,7 +31,7 @@ const CommentSection = ({ post }) => {
     <Typography gutterBottom variant="h6" > Comments </Typography>
     { comments.map((c, i)=> (
         <Typography key={i} gutterBottom variant="subtitle1" >
-        Comment {i}
+        {c}
         </Typography>
     )) }
     </div>
