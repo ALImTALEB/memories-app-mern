93d1b7e1ad846ae70248fb50528805ebc47f2f4f
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-03-04 23:41:42 +0100

    deplo

--- server/index.js
+++ server/index.js
@@ -20,6 +20,8 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
+app.get('/')
+
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
